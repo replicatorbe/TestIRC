dc01ca08c98b3c4da80e727c48736a5a2c08b689
Jerome Fafchamps <smug@smug.fr>
2017-01-20 10:23:05 +0100

    modification

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,30 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/src/browser/Vibration.js",
+        "id": "cordova-plugin-vibration.Vibration",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-legacy-whitelist": "1.1.1"
+    "cordova-plugin-legacy-whitelist": "1.1.1",
+    "cordova-plugin-vibration": "2.1.3",
+    "cordova-plugin-whitelist": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
